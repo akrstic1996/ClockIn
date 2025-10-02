02375558df7dc28b5bd8cfe82e726103a6025a56
akrstic1996 <akrstic1996@gmail.com>
2025-10-02 13:16:30 -0700

    proper time parsing

--- src/main/java/org/krstic/Main.java
+++ src/main/java/org/krstic/Main.java
@@ -31,15 +31,20 @@ public class Main {
                 String ttStr = String.valueOf(totalTimeElapsed);
                 Files.write(Paths.get("src/main/resources/time.txt"), ttStr.getBytes());
                 int seconds = (int) (timeElapsed % 60);
-                int minutes = (int) (timeElapsed / 60);
-                System.out.println("Clocking out\n\n Time Elapsed: " + minutes + " minutes and " + seconds + " seconds." + "Total time " +
-                        totalTimeElapsed + "\n\nEnter anything to clock back in." );
+                int minutes = (int) (timeElapsed / 60) % 60;
+                int hours = (int) (timeElapsed / 360);
+
+                int tseconds = (int) (totalTimeElapsed % 60);
+                int tminutes = (int) (totalTimeElapsed / 60) % 60;
+                int thours = (int) (totalTimeElapsed / 360);
+                System.out.println("Clocking out\n\nTime Elapsed: " + hours + " hours and " + minutes + " minutes and " + seconds + " seconds." + "Total time: " +
+                        thours + " hours " + tminutes + " minutes and " + tseconds + " seconds.\n\nEnter anything to clock back in." );
 
 
 
                 isClockedIn = false;
             } else {
-                System.out.println("Clocking in!\n\n Enter anything to clock out.");
+                System.out.println("Clocking in!\n\nEnter anything to clock out.");
                 isClockedIn = true;
                 timeClockedIn = LocalDateTime.now();
             }
